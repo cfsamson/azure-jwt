a2992d2a5e8e85892598507ef8bf955bfa5c682b
Carl Fredrik Samson <cf@samson.no>
2019-04-15 01:32:00 +0200

    decided to not map the jwt token

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,6 @@ pub struct AzureAuth {
     exp_hours: i64,
     retry_counter: u32,
     retry_option: bool,
-    simple_token: Option<AuthToken>,
 }
 
 impl AzureAuth {
@@ -72,26 +71,14 @@ impl AzureAuth {
             exp_hours: 24,
             retry_counter: 0,
             retry_option: true,
-            simple_token: None,
         })
     }
 
     pub fn validate_token(&mut self, token: &str) -> Result<Token<Header, Claims>, AuthErr> {
         let decoded = self.validate_token_authenticity(token)?;
         let decoded = self.validate_aud(decoded)?;
-        self.simple_token = Some(AuthToken {
-            header: AuthHeader::from(&decoded.header),
-            claims: AuthClaims::from(&decoded.claims),
-        });
 
         Ok(decoded)
-    }
-
-    pub fn simple_token(&self) -> Option<&AuthToken> {
-        match &self.simple_token{
-            Some(t) => Some(t),
-            None => None,
-        }
     }
 
     fn validate_token_authenticity(
@@ -212,65 +199,7 @@ impl AzureAuth {
     }
 }
 
-/// Simplified version of the JWT struct with the most important information for further processing of
-/// Azure JWT tokens
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct AuthToken {
-    pub header: AuthHeader,
-    pub claims: AuthClaims,
-}
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct AuthClaims {
-    pub iss: Option<String>,
-    pub sub: Option<String>,
-    pub aud: Option<String>,
-    pub exp: Option<u64>,
-    pub nbf: Option<u64>,
-    pub iat: Option<u64>,
-    pub jti: Option<String>,
-}
 
-impl From<&Claims> for AuthClaims {
-    fn from(c: &Claims) -> Self {
-        AuthClaims {
-            iss: c.reg.iss.clone(),
-            sub: c.reg.sub.clone(),
-            aud: c.reg.aud.clone(),
-            exp: c.reg.exp,
-            nbf: c.reg.nbf,
-            iat: c.reg.iat,
-            jti: c.reg.jti.clone(),
-        }
-    }
-}
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct AuthHeader {
-    pub typ: Option<String>,
-    pub kid: Option<String>,
-    pub alg: String,
-}
-
-impl From<&Header> for AuthHeader {
-    fn from (h: &Header) -> Self {
-        let typ = match &h.typ {
-            Some(t) => match t {
-                HeaderType::JWT => Some("JWT".to_string()),
-            },
-            None => None,
-        };
-
-        let alg = match h.alg {
-            Algorithm::HS256 => "HS256",
-            Algorithm::RS256 => "Rs256",
-        };
-        
-        AuthHeader {
-            typ,
-            kid: h.kid.clone(),
-            alg: alg.to_string(),
-        }
-    }
-}
 
 #[derive(Debug, Deserialize)]
 struct Keys {
